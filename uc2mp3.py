f68004d1b5ff6acee016ac9e9a96adc44b494b6d
medioqrity <2360138960@qq.com>
2019-06-26 19:26:39 +0800

    cleared testing files

--- uc2mp3.py
+++
@@ -1,8 +0,0 @@
-from glob import glob
-for _ in glob("*.uc"):
-    with open(_, 'rb') as f:
-        data = list(f.read())
-        for i in range(len(data)):
-            data[i] = chr(ord(data[i]) ^ 0xa3)
-        with open(_[:-3] + ".mp3", 'wb') as f2:
-            f2.write(''.join(data))
