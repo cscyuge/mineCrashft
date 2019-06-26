85f4c76ae97dda487bc01d3188807576b8945fed
medioqrity <2360138960@qq.com>
2019-06-26 22:46:01 +0800

    Reformatted Main.java; Replaced tabs to spaces

--- lwjgl tutorial/src/main/Main.java
+++ lwjgl tutorial/src/main/Main.java
@@ -1,39 +1,34 @@
 package main;
 
-public class Main implements Runnable{
-	public Thread game;
-	
-	public void start() {
-		game =new Thread(this,"game");
-		game.start();
-	}
-	
-	public static void init() {
-		
-		System.out.println("hello");
-		
-	}
+public class Main implements Runnable {
+    public Thread game;
 
-	public void update() {
-		System.out.println("updating");
-		
-	}
-	public void render() {
-		System.out.println("ring");
-		
-	}
-	
-	public void run() {
-		init();
-		while (true) {
-			update();
-			render();
-		}
-	}
-	
-	public static void main(String args[]) {
-		 new Main().start();
-		
-		
-	}
+    public void start() {
+        game = new Thread(this, "game");
+        game.start();
+    }
+
+    public static void init() {
+        System.out.println("hello");
+    }
+
+    public void update() {
+        System.out.println("updating");
+    }
+
+    public void render() {
+        System.out.println("ring");
+    }
+
+    public void run() {
+        init();
+        while (true) {
+            update();
+            render();
+        }
+    }
+
+    public static void main(String args[]) {
+        new Main().start();
+    }
 }
